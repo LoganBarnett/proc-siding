c875354b7ce98f1e937ba4293ff2f72294766e93
Logan Barnett <logustus@gmail.com>
2026-03-21 21:17:09 -0700

    ignore shebangs in files that are supposed to have shebangs

--- nix/derivations/mlx-metal-test/default.nix
+++ nix/derivations/mlx-metal-test/default.nix
@@ -1,4 +1,4 @@
 { writers, python3Packages }:
 writers.writePython3Bin "mlx-metal-test"
-  { libraries = [ python3Packages.mlx ]; }
+  { libraries = [ python3Packages.mlx ]; flakeIgnore = [ "E265" ]; }
   (builtins.readFile ./mlx-metal-test)
